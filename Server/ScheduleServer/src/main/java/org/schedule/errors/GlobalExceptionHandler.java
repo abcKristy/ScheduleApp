57370bc9f4ffc1d843db718e2c7e41f29e6ec55e
Kristina <loluta2005@mail.ru>
2025-11-02 15:03:25 +0300

    обработка базовых ошибок

--- Server/ScheduleServer/src/main/java/org/schedule/errors/GlobalExceptionHandler.java
+++ Server/ScheduleServer/src/main/java/org/schedule/errors/GlobalExceptionHandler.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.client.RestClientException;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @RestControllerAdvice
@@ -48,3 +47,51 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
+
+/*
+ * Список ошибок для обработки в проекте:
+ *
+ * 1. Валидация входных данных
+ *    - IllegalArgumentException - пустые списки, null параметры
+ *    - NullPointerException - непроверенные null значения
+ *
+ * 2. Внешние API вызовы
+ *    - RestClientException - ошибки HTTP запросов к MIREA API
+ *    - HttpClientErrorException - 4xx ошибки (404, 400 и т.д.)
+ *    - HttpServerErrorException - 5xx ошибки сервера
+ *    - ResourceAccessException - проблемы с сетью/таймауты
+ *
+ * 3. Работа с базой данных
+ *    - DataAccessException - общие ошибки доступа к данным
+ *    - EmptyResultDataAccessException - данные не найдены
+ *    - DataIntegrityViolationException - нарушение целостности данных
+ *
+ * 4. Парсинг и преобразование данных
+ *    - DateTimeParseException - ошибки парсинга дат из iCal
+ *    - PatternSyntaxException - ошибки в регулярных выражениях
+ *    - NumberFormatException - преобразование строк в числа
+ *
+ * 5. Бизнес-логика
+ *    - RuntimeException - общие ошибки в сервисах
+ *    - IllegalStateException - некорректное состояние приложения
+ *
+ * 6. Транзакции
+ *    - TransactionException - ошибки управления транзакциями
+ *    - TransactionSystemException - системные ошибки транзакций
+ *
+ * 7. Кэширование (когда реализуется)
+ *    - CacheException - ошибки работы с кэшем
+ *    - SerializationException - проблемы сериализации данных
+ *
+ * 8. Общие системные ошибки
+ *    - IOException - ошибки ввода/вывода
+ *    - OutOfMemoryError - нехватка памяти (критическая)
+ *
+ * 9. Специфичные для парсера iCal
+ *    - Exception в parseEventBlock - ошибки парсинга отдельных событий
+ *    - Ошибки валидации isValidLesson - некорректные данные занятий
+ *
+ * 10. Spring Framework ошибки
+ *     - BeanCreationException - ошибки создания бинов
+ *     - NoSuchBeanDefinitionException - бин не найден
+ */
